73927c01c86473a4783023df0de34f9d1bb8f04f
Johnny Console <johnny.console@hotmail.com>
2025-09-17 10:45:21 -0400

    Add basic API class for user operations

---
+++ senvote-restful/src/main/java/com/johnnyconsole/senvote/restful/UserAPI.java
@@ -0,0 +1,10 @@
+package com.johnnyconsole.senvote.restful;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+@Path("/user")
+@RequestScoped
+public class UserAPI {
+
+}
