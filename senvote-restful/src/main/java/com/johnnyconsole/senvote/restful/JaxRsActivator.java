a7e7e2a5ba36b21f5eba25c2a6f986003e776085
Johnny Console <johnny.console@hotmail.com>
2025-09-17 10:45:07 -0400

    Add JAX-RS Activator Class

---
+++ senvote-restful/src/main/java/com/johnnyconsole/senvote/restful/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package com.johnnyconsole.senvote.restful;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+    //Intentionally Blank
+}
