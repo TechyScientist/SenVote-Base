2f24c9fd3fc5255e9e3c56e39cfdfbfb99416191
Johnny Console <johnny.console@hotmail.com>
2025-09-17 13:35:57 -0400

    Create POST method for validating user sign in credentials

--- senvote-restful/src/main/java/com/johnnyconsole/senvote/restful/UserAPI.java
+++ senvote-restful/src/main/java/com/johnnyconsole/senvote/restful/UserAPI.java
@@ -1,10 +1,51 @@
 package com.johnnyconsole.senvote.restful;
 
+import com.johnnyconsole.senvote.persistence.User;
+import com.johnnyconsole.senvote.persistence.interfaces.UserDaoLocal;
+
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.*;
 
 @Path("/user")
 @RequestScoped
 public class UserAPI {
 
+    @EJB private UserDaoLocal userDao;
+    @POST
+    @Path("/signin")
+    @Produces(APPLICATION_JSON)
+    public Response signin(
+            @NotNull @NotEmpty @FormParam("username") String username,
+            @NotNull @NotEmpty @FormParam("password") String password) {
+        String response = "{\n";
+        User user = userDao.getUser(username);
+        if(user == null) {
+            response += "\t\"status\": 404\n";
+            response += "\t\"message\": \"User '" + username + "' not found\"\n";
+        }
+        else {
+            if(user.verifyPassword(password)) {
+                response += "\t\"status\": 200\n";
+                response += "\t\"user\": {\n";
+                response += "\t\t\"username\": \"" + username + "\"\n";
+                response += "\t\t\"name\": \"" + user.name + "\"\n";
+                response += "\t\t\"access\": " + user.accessLevel + "\n";
+                response += "\t}\n";
+            }
+            else {
+                response += "\t\"status\": 401\n";
+                response += "\t\"message\": \"\"Incorrect password for user '" + username + "'\"\n";
+            }
+        }
+        return Response.ok(response + "}").build();
+    }
 }
