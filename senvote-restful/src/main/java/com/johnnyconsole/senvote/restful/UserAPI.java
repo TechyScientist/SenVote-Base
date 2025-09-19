38e8fce224706f0c3f6d971581cbbd148fb42017
Johnny Console <johnny.console@hotmail.com>
2025-09-19 10:47:51 -0400

    Actually hash user passwords when added through the API

--- senvote-restful/src/main/java/com/johnnyconsole/senvote/restful/UserAPI.java
+++ senvote-restful/src/main/java/com/johnnyconsole/senvote/restful/UserAPI.java
@@ -1,10 +1,12 @@
 package com.johnnyconsole.senvote.restful;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.johnnyconsole.senvote.persistence.User;
 import com.johnnyconsole.senvote.persistence.interfaces.UserDaoLocal;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.FormParam;
@@ -20,15 +22,16 @@ import static javax.ws.rs.core.MediaType.*;
 public class UserAPI {
 
     @EJB private UserDaoLocal userDao;
+
     @POST
     @Path("/signin")
     @Produces(APPLICATION_JSON)
     public Response signin(
-            @NotNull @NotEmpty @FormParam("username") String username,
-            @NotNull @NotEmpty @FormParam("password") String password) {
+            @NotEmpty @FormParam("username") String username,
+            @NotEmpty @FormParam("password") String password) {
         String response = "{\n";
         User user = userDao.getUser(username);
-        if(user == null) {
+        if(!userDao.userExists(username)) {
             response += "\t\"status\": 404,\n";
             response += "\t\"category\": \"Not Found\",\n";
             response += "\t\"message\": \"No SenVote record found for '" + username + "'\"\n";
@@ -48,6 +51,58 @@ public class UserAPI {
                 response += "\t\"category\": \"Unauthorized\",\n";
                 response += "\t\"message\": \"Incorrect password\"\n";
             }
+        }
+        return Response.ok(response + "}").build();
+    }
+
+    @POST
+    @Path("/add")
+    @Produces(APPLICATION_JSON)
+    public Response add(@NotEmpty @FormParam("username") String username,
+                        @NotEmpty @FormParam("name") String name,
+                        @NotEmpty @FormParam("password") String password,
+                        @NotEmpty @FormParam("confirm") String confirm,
+                        @FormParam("access") int access,
+                        @FormParam("active") boolean active,
+                        @NotEmpty @FormParam("admin_username") String adminUsername) {
+
+        String response = "{\n";
+        try {
+            if (userDao.userExists(adminUsername)) {
+                User admin = userDao.getUser(adminUsername);
+                if (admin.accessLevel == 1 && admin.accountActive) {
+                    if (!userDao.userExists(username)) {
+                        if (password.equals(confirm)) {
+                            User user = new User(username, name,
+                                    BCrypt.with(BCrypt.Version.VERSION_2A)
+                                            .hashToString(12, password.toCharArray()), access, active);
+                            userDao.addUser(user);
+                            response += "\t\"status\": 200,\n";
+                            response += "\t\"message\": \"User '" + username + "' added to SenVote successfully\"\n";
+                        } else {
+                            response += "\t\"status\": 409,\n";
+                            response += "\t\"category\": \"Conflict\",\n";
+                            response += "\t\"message\": \"Passwords do not match\"\n";
+                        }
+                    } else {
+                        response += "\t\"status\": 409,\n";
+                        response += "\t\"category\": \"Conflict\",\n";
+                        response += "\t\"message\": \"An existing SenVote record was found for user '" + username + "'\"\n";
+                    }
+                } else {
+                    response += "\t\"status\": 401,\n";
+                    response += "\t\"category\": \"Unauthorized\",\n";
+                    response += "\t\"message\": \"User '" + adminUsername + "' is not a SenVote Administrator, or their account is inactive\"\n";
+                }
+            } else {
+                response += "\t\"status\": 404,\n";
+                response += "\t\"category\": \"Not Found\",\n";
+                response += "\t\"message\": \"No SenVote record found for '" + adminUsername + "'\"\n";
+            }
+        } catch(Exception e) {
+            response += "\t\"status\": 400,\n";
+            response += "\t\"category\": \"Bad Request\",\n";
+            response += "\t\"message\": \"Missing or empty parameter\n";
         }
         return Response.ok(response + "}").build();
     }
